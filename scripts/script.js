02fd8081ef7b4c70d14292d483b7b81cecd95a7a
eux <ixue@163.com>
2016-05-27 23:05:17 +0800

    Add hover random background color.

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,74 @@
 
-var gridNum = 16;
 
 $(document).ready(function() {
+
+	$("body").append("<button>Let's Play!</button>");
+
 	$("body").append("<div class='container'></div>");
-	for(var i = 1; i <= gridNum; i++) {
-		$(".container").append("<div class='row'></div>");
-	}
 
-
-	for (var j = 1; j <= gridNum; j++) {
-		$(".row").append("<div class='grid'>hello</div>");
-	}
-
-	$(".grid").mouseenter(function() {
-		$(this).addClass("hover-color");
+	$("button").click(function() {
+		$(".container").empty();
+		gridNum = prompt("How many grids do you want? ^");
+		addGrid(gridNum);
 	});
 
-	$(".grid").mouseleave(function() {
-		$(this).removeClass("hover-color");
-	});
+	addGrid(gridNum);
 
 });
-//console.log("hi");
+
+	var gridNum = 16;
+
+	var addGrid = function(num) {
+
+		for(var i = 1; i <= num; i++) {
+			$(".container").append("<div class='row'></div>");
+		}
+
+		for (var j = 1; j <= num; j++) {
+			$(".row").append("<div class='grid'></div>");
+		}
+
+		$(".grid").mouseenter(function() {
+			$(this).addClass("hover-color");
+			$(".hover-color, .current").css("background", ranColor());
+		});
+
+		$(".grid").mouseleave(function() {
+			$(this).removeClass("hover-color");
+		});
+
+	};
+
+	var ranHex = function() {
+		var ranNum = Math.floor(Math.random()*16);
+		switch(ranNum) {
+			case 10:
+			ranNum = "a";
+			return ranNum;;
+
+			case 11:
+			ranNum = "b";
+			return ranNum;;
+
+			case 12:
+			ranNum = "c";
+			return ranNum;;
+
+			case 13:
+			ranNum = "d";
+			return ranNum;;
+			case 14:
+			ranNum = "e";
+			return ranNum;;
+			case 15:
+			ranNum = "f";
+			return ranNum;;
+			default:
+			return ranNum;;
+		}
+	};
+
+	var ranColor = function() {
+		console.log("#" + ranHex() + ranHex() + ranHex());
+		return "#" + ranHex() + ranHex() + ranHex();
+	};
